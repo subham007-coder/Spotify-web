c222d5dc4501f16a80df26275c032b5d97ba3387
subham007-coder <dsubham109@gmail.com>
2023-11-02 21:43:16 +0530

    work

--- spotify.js
+++ spotify.js
@@ -4,9 +4,15 @@ let pauseBtn = "fa-pause";
 
 let start = document.querySelector(".start-time");
 
+let secTime = 0;
+
+let minTime = 1;
+
+let song = new Audio('songs/Chaleya.mp3');
 
 playBtn.addEventListener("click", () => {
     if (song.paused || song.currentTime <= 0) {
+        console.log("start");
         playFunc();
         playBtn.classList.remove("fa-circle-play");
         playBtn.classList.remove("play");
@@ -21,27 +27,47 @@ playBtn.addEventListener("click", () => {
 
 });
 
-let secTime = 0;
-
-let minTime = 1;
-
-let song = new Audio('songs/Chaleya.mp3');
 
 let playFunc = function () {
 
     song.play();
 
-    let startIntra = setInterval(() => {
-        secTime += 1;
+    // let startIntra = setInterval(() => {
+    //     secTime += 1;
 
-        start.textContent = `0:${secTime}`;
-        console.log(`0:${secTime}`);
+    //     start.textContent = `0:${secTime}`;
+    //     console.log(`0:${secTime}`);
 
-        if (secTime == `60`) {
-            start.textContent = `${minTime}:00`;
-            console.log(`${minTime}:00`);
-            console.log(minTime);
-            clearInterval(startIntra);
-        }
-    }, 1000);
+    //     if (secTime == `60`) {
+    //         start.textContent = `${minTime}:00`;
+    //         console.log(`${minTime}:00`);
+    //         console.log(minTime);
+    //         clearInterval(startIntra);
+    //     }
+    // }, 1000);
 }
+
+let heart = document.querySelector(".fa-heart");
+
+let green_heart = document.querySelector("#green_heart");
+
+let sikBar = document.querySelector(".progress-bar");
+
+heart.addEventListener("click", () => {
+    heart.id = "none";
+    green_heart.id = "view";
+})
+green_heart.addEventListener("click", () => {
+    heart.id = "view";
+    green_heart.id = "none";
+})
+
+song.addEventListener("timeupdate", () => {
+
+    let progresBar = parseInt((song.currentTime / song.duration) * 100);
+    sikBar.value = progresBar;
+})
+
+sikBar.addEventListener("change", () => {
+    song.currentTime = sikBar.value * song.duration / 100;
+})
